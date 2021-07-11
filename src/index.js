efc20ef04fa92db40b85b115c1ae0a17c7918e11
Sharad Maurya <sharadmaurya4600@gmail.com>
2021-07-11 18:14:16 +0530

    first

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+const { json } = require("express");
 var http = require("http");
-
 const httpServer = http.createServer(handleServer);
-
-
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Welcome to Dominos!");
+  } else if (req.url === "/contact") {
+    const obj = {
+      phone: "18602100000",
+      email: "guestcaredominos@jublfood.com"
+    };
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/json");
+    res.end(JSON.stringify(obj));
+  } else {
+    res.writeHead(404).end("page not found");
+  }
 }
-
-module.exports = httpServer;
